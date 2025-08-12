bfc79f01c2242f714802ef48c0546a797fe4c9f1
Thephillyhero <wsantana2021@gmail.com>
2025-08-12 06:33:36 -0400

    Update run_verify.ps1

--- run_verify.ps1
+++ run_verify.ps1
@@ -5,7 +5,5 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python src/simulation/live_trading_simulator.py --mode backtest --start 2024-07-01 --end 2024-07-05 --tickers AAPL,MSFT --save
 
-⚠ Make sure to give execute permissions for the .sh script:
 
-chmod +x run_verify.sh
 
